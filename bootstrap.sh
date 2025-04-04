5c7d0f87e092c6cfedd63e903837a6ecec056198
Mateusz Wasilewski <mat.p.wasilewski@gmail.com>
2025-04-04 12:14:02 +0000

    add bootstrap scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,8 +28,7 @@ fail () {
 }
 
 setup_gitconfig () {
-  if ! [ -f git/gitconfig.local.symlink ]
-  then
+  if ! [ -f gitconfig.local.symlink ]; then
     info 'setup gitconfig'
 
     git_credential='cache'
@@ -43,7 +42,14 @@ setup_gitconfig () {
     user ' - What is your github author email?'
     read -e git_authoremail
 
-    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" gitconfig.local.symlink> git/gitconfig.local.symlink
+    cat > gitconfig.local.symlink << EOF
+# Local Git configuration
+[user]
+    name = $git_authorname
+    email = $git_authoremail
+[credential]
+    helper = $git_credential
+EOF
 
     success 'gitconfig'
   fi
